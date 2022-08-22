8fcfa6fc23023252af5101a98eef12c71e3c1003
Phoenexus22 <brwebber04@gmail.com>
2022-08-22 11:10:43 +0930

    Added expense class and fixed errors

--- public/src/structures/expense.js
+++ public/src/structures/expense.js
@@ -1,7 +1,13 @@
 class expense
 {
-    constructor()
+    constructor(name = "N/A", description = "N/A", cost = 0.0, to = "Undisclosed", signatory = "Undisclosed", budgID = "lost")
     {
-        this.id;
+        this.id = randId();
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.to = to;
+        this.signatory = signatory;
+        this.budgID = budgID;
     }
 }
