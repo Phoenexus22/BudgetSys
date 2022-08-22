dee187b3ce33e273272a06be68a8a8031451f864
Phoenexus22 <brwebber04@gmail.com>
2022-08-22 10:15:03 +0930

    reorganised

---
+++ public/src/genfunc.js
@@ -0,0 +1,23 @@
+
+
+function randId(sourceArray = [], startlength = "20")
+{
+    tryagain: while (true){
+        let id = randb64(startlength)
+     
+        for(let i = 0; i < sourceArray.length; i++)
+        {
+            if (sourceArray[i].id == id){;continue tryagain; }
+        }
+        return id;
+    }
+}
+
+function randb64(length) {
+    var randomChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
