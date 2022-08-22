d313f1efece4f54285b821edac2340dfd6aabac2
Phoenexus22 <brwebber04@gmail.com>
2022-08-22 09:39:54 +0930

    fixed import error

--- public/config.js
+++ public/config.js
@@ -1,7 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +10,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+console.log("initialised");
